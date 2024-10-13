e08b926d4749547455ddb68199c4ad77d04efb81
hassan95eb <19h.amini95@gmail.com>
2024-10-13 14:35:46 +0330

    change  app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import TaskItems from "./components/TaskItems";
+import TopForm from "./components/TopForm";
+
 const App = () => {
-  return <>test</>;
+  return (
+    <>
+      <div className="container w-100 h-100 p-3">
+        <div className="row h-100 justify-content-center align-align-items-start">
+          <div className="col-12 col-md-8 col-lg-6 bg-light shadow rounded-3 p-3 h_fit">
+            <TopForm />
+            <TaskItems />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default App;
